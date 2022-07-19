23fc29a9da1517b038c2eaa685830aa36ad46162
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-07-19 22:17:33 +0100

    future trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[allow(dead_code)]
+use std::future::Future;
+
 fn main() {
     println!("Hello, world!");
+}
+
+async fn foo1() -> usize {
+    0
+}
+
+fn foo2() -> impl Future<Output = usize> {
+    async { 0 }
 }
